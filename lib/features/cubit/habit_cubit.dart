6d2bd14b6fee68a3537964eed7a0b2e925e57b36
feras.Oz <ferasosama99@gmail.com>
2026-01-03 11:14:28 +0200

    - building habits cubit

---
+++ lib/features/cubit/habit_cubit.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_tracker/core/helpers/constants.dart';
+import 'package:habit_tracker/core/helpers/habit_storage.dart';
+import 'package:habit_tracker/core/models/habit_model.dart';
+import 'package:habit_tracker/features/cubit/habit_state.dart';
+
+class HabitCubit extends Cubit<HabitState> {
+  HabitCubit() : super(HabitState());
+
+  //fetch habits
+  Future<void> fetchHabits() async {
+    try {
+      emit(state.copyWith(fetchHabitsStatus: RequestsStatus.loading));
+
+      final habits = await HabitStorage.getHabits();
+
+      emit(
+        state.copyWith(
+          fetchHabitsStatus: RequestsStatus.success,
+          habits: habits,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          fetchHabitsStatus: RequestsStatus.error,
+          errorMessage: e.toString(),
+        ),
+      );
+    }
+  }
+
+  // add habit
+  Future<void> addHabit(HabitModel habit) async {
+    final List<HabitModel> updated = List.from(state.habits)..add(habit);
+
+    await HabitStorage.saveHabits(updated);
+
+    emit(state.copyWith(habits: updated));
+  }
+
+  // delete habit
+  Future<void> deleteHabit(String id) async {
+    final updated = state.habits.where((h) => h.id != id).toList();
+
+    await HabitStorage.saveHabits(updated);
+
+    emit(state.copyWith(habits: updated));
+  }
+
+  // update habit
+  Future<void> updateHabit(HabitModel habit) async {
+    final updated = state.habits.map((h) {
+      if (h.id == habit.id) return habit;
+      return h;
+    }).toList();
+
+    await HabitStorage.saveHabits(updated);
+
+    emit(state.copyWith(habits: updated));
+  }
+}
--- lib/features/cubit/habit_state.dart
+++ lib/features/cubit/habit_state.dart
@@ -3,23 +3,23 @@ import 'package:habit_tracker/core/models/habit_model.dart';
 
 class HabitState {
   final RequestsStatus fetchHabitsStatus;
-  final List<HabitModel> habitModel;
+  final List<HabitModel> habits;
   final String? errorMessage;
 
   HabitState({
     this.fetchHabitsStatus = RequestsStatus.initial,
-    this.habitModel = const [],
+    this.habits = const [],
     this.errorMessage,
   });
 
   HabitState copyWith({
     RequestsStatus? fetchHabitsStatus,
-    List<HabitModel>? habitModel,
+    List<HabitModel>? habits,
     String? errorMessage,
   }) {
     return HabitState(
       fetchHabitsStatus: fetchHabitsStatus ?? this.fetchHabitsStatus,
-      habitModel: habitModel ?? this.habitModel,
+      habits: habits ?? this.habits,
       errorMessage: errorMessage ?? this.errorMessage,
     );
   }
