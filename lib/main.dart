bef1a5753fdf7e1e8a47347dbd930070bf6ba7de
feras.Oz <ferasosama99@gmail.com>
2026-01-05 11:05:49 +0200

    - adding habit storage init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:habit_tracker/core/helpers/habit_storage.dart';
 import 'package:habit_tracker/core/routing/app_routers.dart';
 import 'package:habit_tracker/habit_tracker_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await HabitStorage.init();
   await ScreenUtil.ensureScreenSize();
   runApp(HabitTrackerApp(appRouters: AppRouters()));
 }
