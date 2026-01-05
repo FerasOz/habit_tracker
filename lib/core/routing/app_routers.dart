a6fa7bb837813e18c13f1ef1a6e6f450f8c410f2
feras.Oz <ferasosama99@gmail.com>
2026-01-05 10:59:25 +0200

    - adding logic to home screen

--- lib/core/routing/app_routers.dart
+++ lib/core/routing/app_routers.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_tracker/core/models/habit_model.dart';
 import 'package:habit_tracker/core/routing/routes.dart';
 import 'package:habit_tracker/features/add_habit/add_habit_screen.dart';
+import 'package:habit_tracker/features/cubit/habit_cubit.dart';
 import 'package:habit_tracker/features/habit_details/habit_details_screen.dart';
 import 'package:habit_tracker/features/home/home_screen.dart';
 import 'package:habit_tracker/features/settings/settings_screen.dart';
@@ -10,7 +12,12 @@ class AppRouters {
   Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.homeScreen:
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => HabitCubit()..fetchHabits(),
+            child: HomeScreen(),
+          ),
+        );
       case Routes.addHabitScreen:
         return MaterialPageRoute(builder: (_) => AddHabitScreen());
       case Routes.habitDetailsScreen:
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:habit_tracker/core/helpers/constants.dart';
 import 'package:habit_tracker/core/routing/routes.dart';
 import 'package:habit_tracker/core/styles/colors.dart';
+import 'package:habit_tracker/features/cubit/habit_cubit.dart';
+import 'package:habit_tracker/features/cubit/habit_state.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,7 +21,7 @@ class HomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "My Habits",
+              "Habits Tracker",
               style: GoogleFonts.poppins(
                 fontSize: 22.sp,
                 fontWeight: FontWeight.w600,
@@ -48,20 +52,41 @@ class HomeScreen extends StatelessWidget {
         child: const Icon(Icons.add, color: Colors.white),
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            _buildSummaryCard(),
-            const SizedBox(height: 20),
-            Expanded(child: _buildHabitsList()),
-          ],
-        ),
+      body: BlocBuilder<HabitCubit, HabitState>(
+        builder: (context, state) {
+          if (state.fetchHabitsStatus == RequestsStatus.loading) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (state.habits.isEmpty) {
+            return Center(child: Text("No habits yet"));
+          }
+
+          final total = state.habits.length;
+          final doneToday = state.habits.where((h) => h.doneToday).length;
+          final double progress = total == 0 ? 0 : doneToday / total;
+
+          return RefreshIndicator(
+            onRefresh: () {
+              return context.read<HabitCubit>().fetchHabits();
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  _buildSummaryCard(doneToday, total, progress),
+                  const SizedBox(height: 20),
+                  Expanded(child: _buildHabitsList(state)),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 
-  Widget _buildSummaryCard() {
+  Widget _buildSummaryCard(int done, int total, double progress) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(20),
@@ -78,90 +103,49 @@ class HomeScreen extends StatelessWidget {
           ),
           const SizedBox(height: 10),
           Text(
-            "3 / 5 Habits Completed",
+            "$done / $total Habits Completed",
             style: GoogleFonts.poppins(
               color: Colors.white,
               fontSize: 22.sp,
               fontWeight: FontWeight.w600,
             ),
           ),
+          SizedBox(height: 10),
+          LinearProgressIndicator(
+            value: progress,
+            backgroundColor: Colors.white30,
+            valueColor: AlwaysStoppedAnimation(Colors.white),
+          ),
         ],
       ),
     );
   }
 
-  Widget _buildHabitsList() {
+  Widget _buildHabitsList(HabitState state) {
     return ListView.separated(
-      itemCount: 3,
-      separatorBuilder: (_, _) => SizedBox(height: 12.h),
+      itemCount: state.habits.length,
+      separatorBuilder: (_, __) => SizedBox(height: 12.h),
       itemBuilder: (context, index) {
-        return _habitCard(
-          icon: Icons.water_drop,
-          title: "Drink Water",
-          progress: "2 / 8",
-          value: 0.25,
+        final habit = state.habits[index];
+
+        return ListTile(
           onTap: () {
-            Navigator.pushNamed(context, Routes.habitDetailsScreen,
-                arguments: null);
+            Navigator.pushNamed(
+              context,
+              Routes.habitDetailsScreen,
+              arguments: habit,
+            );
           },
+          leading: Icon(
+            habit.doneToday ? Icons.check_circle : Icons.circle_outlined,
+            color: habit.doneToday ? Colors.green : Colors.grey,
+          ),
+          title: Text(habit.title),
+          subtitle: habit.description == null || habit.description!.isEmpty
+              ? null
+              : Text(habit.description!),
         );
       },
-    );
-  }
-
-  Widget _habitCard({
-    required IconData icon,
-    required String title,
-    required String progress,
-    required double value,
-    required Function()? onTap,
-  }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(18.r),
-        ),
-        child: Row(
-          children: [
-            CircleAvatar(
-              radius: 24,
-              backgroundColor: const Color(0xFFEDEBFF),
-              child: Icon(icon, color: ColorsManager.primaryColor),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: GoogleFonts.poppins(
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(height: 6),
-                  LinearProgressIndicator(
-                    value: value,
-                    backgroundColor: Colors.grey.shade200,
-                    valueColor: const AlwaysStoppedAnimation(
-                      ColorsManager.primaryColor,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(width: 12),
-            Text(
-              progress,
-              style: GoogleFonts.poppins(fontSize: 14.sp, color: Colors.grey),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
