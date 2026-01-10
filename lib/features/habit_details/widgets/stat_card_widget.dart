7d9a08e73a051127511c8a63604f96bf8a4b7251
feras.Oz <ferasosama99@gmail.com>
2026-01-10 11:07:55 +0200

    - Refactoring stat card widget

---
+++ lib/features/habit_details/widgets/stat_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:habit_tracker/core/styles/colors.dart';
+
+class StatCardWidget extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+  
+  const StatCardWidget({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.r),
+        ),
+        child: Column(
+          children: [
+            Icon(icon, color: ColorsManager.primaryColor),
+            const SizedBox(height: 8),
+            Text(
+              value,
+              style: GoogleFonts.poppins(
+                fontSize: 18.sp,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(title, style: GoogleFonts.poppins(color: Colors.grey)),
+          ],
+        ),
+      ),
+    );
+  }
+}
