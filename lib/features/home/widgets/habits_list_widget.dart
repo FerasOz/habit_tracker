7f0532aa6aee5b649f279e0367671afb93398fc6
feras.Oz <ferasosama99@gmail.com>
2026-01-17 11:32:25 +0200

    - adding locale keys to habits list

--- lib/features/home/widgets/habits_list_widget.dart
+++ lib/features/home/widgets/habits_list_widget.dart
@@ -1,8 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:habit_tracker/core/helpers/spacing.dart';
 import 'package:habit_tracker/features/cubit/habit_state.dart';
 import 'package:habit_tracker/features/home/widgets/habit_card_widget.dart';
+import 'package:habit_tracker/generated/locale_keys.g.dart';
 
 class HabitsListWidget extends StatelessWidget {
   final HabitState state;
@@ -14,7 +16,7 @@ class HabitsListWidget extends StatelessWidget {
     if (state.habits.isEmpty) {
       return Center(
         child: Text(
-          "No habits yet",
+          LocaleKeys.home_emptyHabits.tr(),
           style: GoogleFonts.poppins(color: Colors.grey),
         ),
       );
