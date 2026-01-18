605e203028b577bf0f435af8a7f84995a1f69dda
feras.Oz <ferasosama99@gmail.com>
2026-01-18 09:55:56 +0200

    - adding change language method

--- lib/features/cubit/habit_cubit.dart
+++ lib/features/cubit/habit_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_tracker/core/helpers/constants.dart';
@@ -64,5 +65,11 @@ class HabitCubit extends Cubit<HabitState> {
     await HabitStorage.saveHabits(updated);
 
     emit(state.copyWith(habits: updated));
+  }
+
+  // change language
+  Future<void> changeLanguage(BuildContext context, Locale locale) async {
+    await context.setLocale(locale);
+    emit(state.copyWith(locale: locale));
   }
 }
--- lib/features/cubit/habit_state.dart
+++ lib/features/cubit/habit_state.dart
@@ -1,25 +1,30 @@
+import 'dart:ui';
 import 'package:habit_tracker/core/helpers/constants.dart';
 import 'package:habit_tracker/core/models/habit_model.dart';
 
 class HabitState {
   final RequestsStatus fetchHabitsStatus;
   final List<HabitModel> habits;
+  final Locale locale;
   final String? errorMessage;
 
   HabitState({
     this.fetchHabitsStatus = RequestsStatus.initial,
     this.habits = const [],
+    this.locale = const Locale('en'),
     this.errorMessage,
   });
 
   HabitState copyWith({
     RequestsStatus? fetchHabitsStatus,
     List<HabitModel>? habits,
+    Locale? locale,
     String? errorMessage,
   }) {
     return HabitState(
       fetchHabitsStatus: fetchHabitsStatus ?? this.fetchHabitsStatus,
       habits: habits ?? this.habits,
+      locale: locale ?? this.locale,
       errorMessage: errorMessage ?? this.errorMessage,
     );
   }
