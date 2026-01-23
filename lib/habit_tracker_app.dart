e8450f8aa8ea90772ec88f77d133ef21f41fbb97
feras.Oz <ferasosama99@gmail.com>
2026-01-23 10:55:40 +0200

    - Removing multi bloc provider

--- lib/habit_tracker_app.dart
+++ lib/habit_tracker_app.dart
@@ -1,10 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:habit_tracker/core/routing/app_routers.dart';
 import 'package:habit_tracker/core/routing/routes.dart';
-import 'package:habit_tracker/features/cubit/habit_cubit.dart';
 
 class HabitTrackerApp extends StatelessWidget {
   final AppRouters appRouters;
@@ -12,26 +10,23 @@ class HabitTrackerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => HabitCubit()..loadSavedLanguage(context),
-      child: ScreenUtilInit(
-        designSize: const Size(393, 852),
-        minTextAdapt: true,
-        builder: (context, child) {
-          return MaterialApp(
-            localizationsDelegates: context.localizationDelegates,
-            supportedLocales: context.supportedLocales,
-            locale: context.locale,
-            debugShowCheckedModeBanner: false,
-            title: 'Habit Tracker',
-            themeMode: ThemeMode.system,
-            // theme: lightTheme,
-            // darkTheme: darkTheme,
-            onGenerateRoute: appRouters.onGenerateRoute,
-            initialRoute: Routes.homeScreen,
-          );
-        },
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          debugShowCheckedModeBanner: false,
+          title: 'Habit Tracker',
+          themeMode: ThemeMode.system,
+          // theme: lightTheme,
+          // darkTheme: darkTheme,
+          onGenerateRoute: appRouters.onGenerateRoute,
+          initialRoute: Routes.homeScreen,
+        );
+      },
     );
   }
 }
