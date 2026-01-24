cd3f1db7150aee2c27ad21f9463c8567fa445b6b
feras.Oz <ferasosama99@gmail.com>
2026-01-24 11:18:40 +0200

    - editting habit model

--- lib/data/models/habit_model.dart
+++ lib/data/models/habit_model.dart
@@ -3,22 +3,16 @@ class HabitModel {
   final String title;
   final String? description;
   final DateTime createdAt;
-  final int targetPerWeek;
-  final int completedDays;
-  final int currentStreak;
-  final bool doneToday;
   final List<int> activeDays;
+  final Set<String> completedDates;
 
   HabitModel({
     required this.id,
     required this.title,
     this.description,
     required this.createdAt,
-    required this.targetPerWeek,
-    required this.completedDays,
-    required this.currentStreak,
-    required this.doneToday,
     required this.activeDays,
+    required this.completedDates,
   });
 
   HabitModel copyWith({
@@ -26,22 +20,16 @@ class HabitModel {
     String? title,
     String? description,
     DateTime? createdAt,
-    int? targetPerWeek,
-    int? completedDays,
-    int? currentStreak,
-    bool? doneToday,
     List<int>? activeDays,
+    Set<String>? completedDates,
   }) {
     return HabitModel(
       id: id ?? this.id,
       title: title ?? this.title,
       description: description ?? this.description,
       createdAt: createdAt ?? this.createdAt,
-      targetPerWeek: targetPerWeek ?? this.targetPerWeek,
-      completedDays: completedDays ?? this.completedDays,
-      currentStreak: currentStreak ?? this.currentStreak,
-      doneToday: doneToday ?? this.doneToday,
       activeDays: activeDays ?? this.activeDays,
+      completedDates: completedDates ?? this.completedDates,
     );
   }
 
@@ -51,11 +39,8 @@ class HabitModel {
       title: map['title'],
       description: map['description'],
       createdAt: DateTime.parse(map['createdAt']),
-      targetPerWeek: map['targetPerWeek'],
-      completedDays: map['completedDays'],
-      currentStreak: map['currentStreak'],
-      doneToday: map['doneToday'],
       activeDays: List<int>.from(map['activeDays']),
+      completedDates: Set<String>.from(map['completedDates'] ?? []),
     );
   }
 
@@ -65,11 +50,8 @@ class HabitModel {
       'title': title,
       'description': description,
       'createdAt': createdAt.toIso8601String(),
-      'targetPerWeek': targetPerWeek,
-      'completedDays': completedDays,
-      'currentStreak': currentStreak,
-      'doneToday': doneToday,
       'activeDays': activeDays,
+      'completedDates': completedDates.toList(),
     };
   }
 }
